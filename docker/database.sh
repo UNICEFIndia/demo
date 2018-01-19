bf7e264f91650ad47ed9dc01f8db8b75f248cc1c
eltonplima <eltonplima@gmail.com>
2018-01-19 08:30:33 +0530

    Docker infra.

--- docker/database.sh
+++ docker/database.sh
@@ -2,5 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    # Your SQL instructions goes here.
+    CREATE USER demo_user WITH ENCRYPTED PASSWORD 'demo_pass';
+    CREATE DATABASE demo_db;
+    GRANT ALL PRIVILEGES ON DATABASE demo_db TO demo_user;
 EOSQL
